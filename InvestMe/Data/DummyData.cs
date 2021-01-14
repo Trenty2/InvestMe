b30c745f001d20e5d2b49123bbae8b7273d68402
Trent Sargisson <trent8865@gmail.com>
2021-01-14 13:34:07 +0800

    Added DummyData back and changes fixed auto assign issue

--- InvestMe/Data/DummyData.cs
+++ InvestMe/Data/DummyData.cs
@@ -14,72 +14,72 @@ namespace InvestMe.Data
             UserManager<ApplicationUser> userManager,
             RoleManager<ApplicationRole> roleManager)
         {
-            //context.Database.EnsureCreated();
+            context.Database.EnsureCreated();
 
 
-            //string role1 = "Admin";
-            //string desc1 = "This is the administrator role";
+            string role1 = "Admin";
+            string desc1 = "This is the administrator role";
 
-            //string role2 = "Member";
-            //string desc2 = "This is the members role";
+            string role2 = "Member";
+            string desc2 = "This is the members role";
 
-            //string password = "Pogchamp!1";
+            string password = "Pogchamp!1";
 
-            //if (await roleManager.FindByNameAsync(role1) == null)
-            //{
-            //    await roleManager.CreateAsync(new ApplicationRole(role1, desc1, DateTime.Now));
-            //}
+            if (await roleManager.FindByNameAsync(role1) == null)
+            {
+                await roleManager.CreateAsync(new ApplicationRole(role1, desc1, DateTime.Now));
+            }
 
-            //if (await roleManager.FindByNameAsync(role2) == null)
-            //{
-            //    await roleManager.CreateAsync(new ApplicationRole(role2, desc2, DateTime.Now));
-            //}
+            if (await roleManager.FindByNameAsync(role2) == null)
+            {
+                await roleManager.CreateAsync(new ApplicationRole(role2, desc2, DateTime.Now));
+            }
 
-            //if (await userManager.FindByNameAsync("aa@aa.aa") == null)
-            //{
-            //    var user = new ApplicationUser
-            //    {
-            //        UserName = "aa@aa.aa",
-            //        Email = "aa@aa.aa",
-            //        FirstName = "Adam",
-            //        LastName = "Aldridge",
-            //        Street = "Fake St",
-            //        City = "Vancouver",
-            //        Country = "Canada",
-            //        PhoneNumber = "6902341234"
-            //    };
+            if (await userManager.FindByNameAsync("aa@aa.aa") == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "aa@aa.aa",
+                    Email = "aa@aa.aa",
+                    FirstName = "Adam",
+                    LastName = "Aldridge",
+                    Street = "Fake St",
+                    City = "Vancouver",
+                    Country = "Canada",
+                    PhoneNumber = "6902341234"
+                };
 
-            //    var result = await userManager.CreateAsync(user);
-            //    if (result.Succeeded)
-            //    {
-            //        await userManager.AddPasswordAsync(user, password);
-            //        await userManager.AddToRoleAsync(user, role1);
-            //    }
+                var result = await userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await userManager.AddPasswordAsync(user, password);
+                    await userManager.AddToRoleAsync(user, role1);
+                }
 
-            //}
+            }
 
-            //if (await userManager.FindByNameAsync("bb@bb.bb") == null)
-            //{
-            //    var user = new ApplicationUser
-            //    {
-            //        UserName = "bb@bb.bb",
-            //        Email = "bb@bb.bb",
-            //        FirstName = "Bob",
-            //        LastName = "Barker",
-            //        Street = "Vermont St",
-            //        City = "Surrey",
-            //        Country = "Canada",
-            //        PhoneNumber = "7788951456"
-            //    };
+            if (await userManager.FindByNameAsync("bb@bb.bb") == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "bb@bb.bb",
+                    Email = "bb@bb.bb",
+                    FirstName = "Bob",
+                    LastName = "Barker",
+                    Street = "Vermont St",
+                    City = "Surrey",
+                    Country = "Canada",
+                    PhoneNumber = "7788951456"
+                };
 
-            //    var result = await userManager.CreateAsync(user);
-            //    if (result.Succeeded)
-            //    {
-            //        await userManager.AddPasswordAsync(user, password);
-            //        await userManager.AddToRoleAsync(user, role1);
-            //    }
-            //}
-            
+                var result = await userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await userManager.AddPasswordAsync(user, password);
+                    await userManager.AddToRoleAsync(user, role1);
+                }
+            }
+
         }
     }
 
